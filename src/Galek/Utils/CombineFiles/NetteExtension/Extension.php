873cfde7e9aae7920ec6fa28eca131db844a763a
Jan Galek <admin@gcore.cz>
2016-06-01 16:33:15 +0200

    Prepare Class for Nette Extension

---
+++ src/Galek/Utils/CombineFiles/NetteExtension/Extension.php
@@ -0,0 +1,19 @@
+<?php
+
+namespace Galek\Utils\Nette;
+
+use Nette\Configurator;
+use Nette\DI\Compiler;
+use Nette\DI\CompilerExtension;
+use Nette\DI\Config\Helpers;
+use Nette\DI\ContainerBuilder;
+use Nette\Utils\Finder;
+use Nette;
+
+class Extension extends CompilerExtension
+{
+    public function loadConfig()
+    {
+      
+    }
+}
