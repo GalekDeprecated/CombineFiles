3a19a20d98d1789b1daa68b297f6da339ae970a2
Jan Galek <admin@gcore.cz>
2016-10-01 08:42:51 +0200

    Replaced: __destruct to __toString

--- tests/Galek/Basic.php
+++ tests/Galek/Basic.php
@@ -8,23 +8,15 @@ use Tracy\Debugger;
 
 Debugger::enable(__DIR__ . '/log'); // aktivujeme Laděnku
 
-class Basic
-{
-    public function render()
-    {
-        $path = 'css';
-        $root = __DIR__;
-        $t = new Utils\CombineFiles($root, $path);
-        $t->addFile('style_bot.css');
-        $t->addFile('style_top.css');
-        $t->addFile('style_effects.css');
-        $t->addFile('style_critical.css');
-        return $t;
-    }
-}
+$path = 'css';
+$root = __DIR__;
+$t = new Utils\CombineFiles($root, $path);
+$t->addFile('style_bot.css');
+$t->addFile('style_top.css');
+$t->addFile('style_effects.css');
+$t->addFile('style_critical.css');
 
-$basic = new Basic();
 ?>
-<link rel="stylesheet" type="text/css" href="<?php $basic->render(); ?>">
+<link rel="stylesheet" type="text/css" href="<?php echo $t; ?>">
 
 <h1>Tested</h1>
